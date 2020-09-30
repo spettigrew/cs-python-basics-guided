fd7e08609dc6a3b11d988078c754f3cf0c330750
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-09-29 18:52:46 -0600

    Guided project started

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -19,7 +19,39 @@ The assumptions we are making about our input are the following:
 """
 def split_in_parts(s, part_length):
     # Your code here
+    # create a variable for the new string
+    return_list = []
+    print("return list: ", return_list)
+    substring = ""
 
+    # for i in range(0, len(s) + 1, 3): # for (int i = 0 ; i < len(s) + 1; i += 3)
+
+    for letter in s:
+        print(letter)
+        # if the length of the substring is less than part_length:
+        if len(substring) < part_length:
+        # then we add the letter to it
+        substring = substring + letter # || substring += letter
+        # otherwise, we add it the substring to the return list
+        else: 
+            print("substring: " + substring)
+            return_list.append(substring)
+        # reset the substring to be an empty string
+            substring = ""
+        # add the letter
+            substring += letter
+
+    if len(substring) > 0:
+        return_list.append(substring)
+    
+    return_string = " ".join(return_list)
+
+    return return_string
+
+    # for i in range(len(s)):
+
+    # string_slice = s[start_index:end_index]
 
 # Your code here
+split_in_parts("supercalifragilisticexpialidocious", 3)
 
